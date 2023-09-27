f993e9ec2d7d9d2abb037cd5bd835eecb9a9ee27
Ariel Rafi <144279145+ariellweb@users.noreply.github.com>
2023-09-27 05:19:53 +0330

    Add files via upload

---
+++ practice/practice.dart
@@ -0,0 +1,40 @@
+main() {
+  car bmw = car();
+  bmw.start();
+  bucycle docharkhe = bucycle();
+  docharkhe.start();
+
+  var c = car();
+  c.start();
+  var b = car();
+  b.start();
+}
+
+class vehicle {
+  int? speedtop;
+  String? color;
+  double? price;
+
+  void start() {
+    print('vehicle is starting');
+  }
+
+  void stop() {}
+}
+
+class car extends vehicle {
+  @override
+  void start() {
+    super
+        .start(); //baes mishe ke az class pedar ham property biad pakesh konim nemiad
+    print('lets start');
+  }
+}
+
+class bucycle extends vehicle {
+  @override
+  void start() {
+    super.start();
+    print('bike is starting');
+  }
+}
