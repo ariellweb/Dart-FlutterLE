2e2b6afca8ff137bcc22ca898156c8f1da06ed87
Ariel Rafi <144279145+ariellweb@users.noreply.github.com>
2024-02-18 00:05:18 +0330

    Add files via upload

---
+++ moror19.dart/Moror19syncAsyncReturntype.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) async{
+  print('downloding..');
+  String? response = await getData();
+  showData(response);
+}
+
+Future<String?> getData() async{
+  var delay = Duration(seconds: 3);
+  String? result;
+  Future.delayed(delay, () {
+    result = 'data:ariel height;170 weight:160';
+  });
+  return result;
+}
+
+showData(String? data) {
+  print(data);
+}
